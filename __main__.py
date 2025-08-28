587fdca59f2321a8eebeb87e68a6bff88dee8569
hitmeee666-lang <hitmeee666@gmail.com>
2025-08-28 03:26:24 -0400

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -13,13 +13,12 @@ if choice == "1":
     pdf = input("Enter PDF file path:")
     docs = input("Enter output Docx file path:")
     pdf_to_docx.pdf_to_docx(pdf, docs)
-    print("Conversion Complete")
-
+    print(f"Conversion Complete: ", pdf_to_docx.pdf_tpo_docx(pdf, docs))
 elif choice == "2":
     docx = input("Enter Docx file path:")
     pdf = input("Enter output PDF file path:")
     docx_to_pdf.docx_to_pdf(docx, pdf)
-    print("Conversion Complete")
+    print(f"Comversion Complete: ", docx_to_pdf.docx_to_pdf(docx, pdf))
 
 elif choice == "3":
     print("Closing program...")
