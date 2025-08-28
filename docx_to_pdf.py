27c314f01f7ea77ccf296ce439d3c27e1a95f31e
hitmeee666-lang <hitmeee666@gmail.com>
2025-08-28 03:26:54 -0400

    Update docx_to_pdf.py

--- docx_to_pdf.py
+++ docx_to_pdf.py
@@ -3,7 +3,7 @@ from docx2pdf import convert
 def docx_to_pdf(docx_file, pdf_file):
     try:
         convert(docx_file, pdf_file)
-        return f"[+] Converted {pdf_file} -> {docx_file}"
+        return f"[+] Converted {docx_file} -> {pdf_file}"
     
     except Exception as e:
         return f"[!] Error: {str(e)}"
